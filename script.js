1fb14c7e3aea227822df45711af710f699c906c1
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2024-02-15 16:06:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const byteSize = (str) => {
   // write your code here
-};
+	 let byteSize = 0;
+    
+    // Iterate through each character in the string
+    for (let i = 0; i < str.length; i++) {
+        // Get the Unicode code point of the character
+        const codePoint = str.charCodeAt(i);
+        
+        // Increment byte size based on the number of bytes required to encode the character
+        byteSize += (codePoint < 128) ? 1 : (codePoint < 2048) ? 2 : (codePoint < 65536) ? 3 : 4;
+    }
+    
+    // Return the total byte size of the string
+    return byteSize;
+}
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
